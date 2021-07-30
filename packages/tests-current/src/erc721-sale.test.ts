ad79965cbbfc93cac348c50ddbb3a69af11a0815
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-07-30 07:19:35 +0300

    fix(*): remove runAction

--- packages/tests-current/src/erc721-sale.test.ts
+++ packages/tests-current/src/erc721-sale.test.ts
@@ -6,7 +6,6 @@ import { deployTestErc721, erc721Mint } from "./contracts/test-erc721"
 import { deployTestErc20, erc20Mint } from "./contracts/test-erc20"
 import { awaitAll } from "./common/await-all"
 import { awaitStockToBe } from "./common/await-stock-to-be"
-import { runAction } from "./common/run-action"
 import { verifyErc20Balance } from "./common/verify-erc20-balance"
 import { verifyErc721Owner } from "./common/verify-erc721-owner"
 
@@ -26,26 +25,24 @@ describe("erc721-sale", function() {
 		await erc721Mint(conf.testErc721, wallet1.getAddressString(), wallet1.getAddressString(), 1)
 		await erc20Mint(conf.testErc20, wallet1.getAddressString(), wallet2.getAddressString(), 100)
 
-		const order = await runAction(
-			sdk1.order.sell({
-				makeAssetType: {
-					assetClass: "ERC721",
-					contract: toAddress(conf.testErc721.options.address),
-					tokenId: toBigNumber("1"),
-				},
-				amount: 1,
-				maker: toAddress(wallet1.getAddressString()),
-				originFees: [],
-				payouts: [],
-				price: 10,
-				takeAssetType: { assetClass: "ERC20", contract: toAddress(conf.testErc20.options.address) },
-			}),
-		)
+		const order = await sdk1.order.sell({
+			makeAssetType: {
+				assetClass: "ERC721",
+				contract: toAddress(conf.testErc721.options.address),
+				tokenId: toBigNumber("1"),
+			},
+			amount: 1,
+			maker: toAddress(wallet1.getAddressString()),
+			originFees: [],
+			payouts: [],
+			price: 10,
+			takeAssetType: { assetClass: "ERC20", contract: toAddress(conf.testErc20.options.address) },
+		}).then(a => a.runAll())
 
 		await awaitStockToBe(sdk1.apis.order, order.hash, 1)
 		await verifyErc20Balance(conf.testErc20, wallet2.getAddressString(), 100)
 
-		await runAction(sdk2.order.fill(order, { payouts: [], originFees: [], amount: 1, infinite: true }))
+		await sdk2.order.fill(order, { payouts: [], originFees: [], amount: 1, infinite: true }).then(a => a.runAll())
 
 		await verifyErc20Balance(conf.testErc20, wallet1.getAddressString(), 10)
 		await verifyErc721Owner(conf.testErc721, 1, wallet2.getAddressString())
