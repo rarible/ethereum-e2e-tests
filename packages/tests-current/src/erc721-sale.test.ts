ae099b028ff01ad5640f771037ee0352a3624c05
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-08-01 20:48:53 +0300

    fix(*): test for activity

--- packages/tests-current/src/erc721-sale.test.ts
+++ packages/tests-current/src/erc721-sale.test.ts
@@ -8,6 +8,7 @@ import { awaitAll } from "./common/await-all"
 import { awaitStockToBe } from "./common/await-stock-to-be"
 import { verifyErc20Balance } from "./common/verify-erc20-balance"
 import { verifyErc721Owner } from "./common/verify-erc721-owner"
+import { retry } from "./retry"
 
 describe("erc721-sale", function() {
 	const { web3: web31, wallet: wallet1 } = createE2eProvider()
@@ -48,5 +49,11 @@ describe("erc721-sale", function() {
 		await verifyErc721Owner(conf.testErc721, 1, wallet2.getAddressString())
 
 		await awaitStockToBe(sdk1.apis.order, order.hash, 0)
-	}, 10000)
+
+		await retry(10, async() => {
+			const a = await sdk2.apis.orderActivity.getOrderActivities({ orderActivityFilter: { "@type": "by_item", contract: toAddress(conf.testErc721.options.address), tokenId: toBigNumber("1"), types: ["MATCH", "LIST", "BID"] } })
+			expect(a.items.filter(a => a["@type"] === "match")).toHaveLength(1)
+			expect(a.items.filter(a => a["@type"] === "list")).toHaveLength(1)
+		})
+	}, 30000)
 })
