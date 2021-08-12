56d81585d20cdf3f495190fce467103e992ff759
vitaliyzhalnin <cueage@gmail.com>
2021-08-12 13:23:50 +0700

    fix sdk initialization

--- packages/tests-current/src/create-bid.test.ts
+++ packages/tests-current/src/create-bid.test.ts
@@ -2,6 +2,7 @@ import { createRaribleSdk } from "@rarible/protocol-ethereum-sdk"
 import { Order } from '@rarible/protocol-api-client'
 import fetch from "node-fetch"
 import { toAddress, toBigNumber } from "@rarible/types"
+import { Web3Ethereum } from "@rarible/web3-ethereum"
 import { createE2eProvider } from "./common/create-e2e-provider"
 import { deployTestErc721, erc721Mint } from "./contracts/test-erc721"
 import { deployTestErc20, erc20Mint } from "./contracts/test-erc20"
@@ -15,8 +16,8 @@ describe("erc721 create bid/accept bid", function () {
 	const { web3: web31, wallet: wallet1 } = createE2eProvider()
 	const { web3: web32, wallet: wallet2 } = createE2eProvider()
 
-	const sdk1 = createRaribleSdk(web31, "e2e", { fetchApi: fetch })
-	const sdk2 = createRaribleSdk(web32, "e2e", { fetchApi: fetch })
+	const sdk1 = createRaribleSdk(new Web3Ethereum({ web3: web31 }), "e2e", { fetchApi: fetch })
+	const sdk2 = createRaribleSdk(new Web3Ethereum({ web3: web32 }), "e2e", { fetchApi: fetch })
 
 	const conf = awaitAll({
 		testErc20: deployTestErc20(web31),
