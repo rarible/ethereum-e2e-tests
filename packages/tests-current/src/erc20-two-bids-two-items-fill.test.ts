c239066b2c4402b18d58390e967168cb6982d9d6
rustrobot <a.sedykh@rarible.com>
2021-11-12 15:35:13 +0300

    added_test_for_infinite_bids

---
+++ packages/tests-current/src/erc20-two-bids-two-items-fill.test.ts
@@ -0,0 +1,117 @@
+import {createRaribleSdk} from "@rarible/protocol-ethereum-sdk"
+import {Order, OrderActivityFilterByItemTypes} from "@rarible/ethereum-api-client"
+import {toAddress, toBigNumber} from "@rarible/types"
+import {Web3Ethereum} from "@rarible/web3-ethereum"
+import {RaribleV2OrderFillRequest} from "@rarible/protocol-ethereum-sdk/build/order/fill-order/types"
+import {deployTestErc721, erc721Mint} from "./contracts/test-erc721"
+import {deployTestErc20, erc20Mint} from "./contracts/test-erc20"
+import {awaitAll} from "./common/await-all"
+import {awaitStockToBe} from "./common/await-stock-to-be"
+import {verifyErc20Balance} from "./common/verify-erc20-balance"
+import {verifyErc721Owner} from "./common/verify-erc721-owner"
+import {retry} from "./common/retry"
+import {initProviders} from "./common/init-providers"
+
+describe("Create two ERC20 bid for two ERC721 items and fill them", function () {
+	const { web31, web32, wallet1, wallet2 } = initProviders({})
+
+	const sdk1 = createRaribleSdk(new Web3Ethereum({ web3: web31 }), "e2e")
+	const sdk2 = createRaribleSdk(new Web3Ethereum({ web3: web32 }), "e2e")
+
+	const conf = awaitAll({
+		testErc20: deployTestErc20(web31),
+		testErc721: deployTestErc721(web31),
+	})
+
+	test("test create/accept bid of erc721", async () => {
+		await erc20Mint(conf.testErc20, wallet1.getAddressString(), wallet1.getAddressString(), 100)
+		await erc721Mint(conf.testErc721, wallet1.getAddressString(), wallet2.getAddressString(), 1)
+		await erc721Mint(conf.testErc721, wallet1.getAddressString(), wallet2.getAddressString(), 2)
+
+		const order: Order = await sdk1.order.bid.start({
+			makeAssetType: {
+				assetClass: "ERC20",
+				contract: toAddress(conf.testErc20.options.address),
+			},
+			takeAssetType: {
+				assetClass: "ERC721",
+				contract: toAddress(conf.testErc721.options.address),
+				tokenId: toBigNumber("1"),
+			},
+			amount: 1,
+			maker: toAddress(wallet1.getAddressString()),
+			originFees: [],
+			payouts: [],
+			price: "1",
+		}).runAll()
+
+		const order1: Order = await sdk1.order.bid.start({
+			makeAssetType: {
+				assetClass: "ERC20",
+				contract: toAddress(conf.testErc20.options.address),
+			},
+			takeAssetType: {
+				assetClass: "ERC721",
+				contract: toAddress(conf.testErc721.options.address),
+				tokenId: toBigNumber("2"),
+			},
+			amount: 1,
+			maker: toAddress(wallet1.getAddressString()),
+			originFees: [],
+			payouts: [],
+			price: "1",
+		}).runAll()
+
+
+		await awaitStockToBe(sdk1.apis.order, order.hash, 1)
+		await verifyErc20Balance(conf.testErc20, wallet1.getAddressString(), 100)
+
+		await sdk2.order.fill.start({
+			order,
+			originFee: 0,
+			amount: 1,
+			infinite: true,
+		} as RaribleV2OrderFillRequest).runAll()
+
+		await sdk2.order.fill.start({
+			order: order1,
+			originFee: 0,
+			amount: 1,
+			infinite: true,
+		} as RaribleV2OrderFillRequest).runAll()
+
+		await verifyErc20Balance(conf.testErc20, wallet1.getAddressString(), 98)
+		await verifyErc20Balance(conf.testErc20, wallet2.getAddressString(), 2)
+		await verifyErc721Owner(conf.testErc721, 1, wallet1.getAddressString())
+		await verifyErc721Owner(conf.testErc721, 2, wallet1.getAddressString())
+		await awaitStockToBe(sdk1.apis.order, order.hash, 0)
+
+		await retry(10, async () => {
+			const a = await sdk2.apis.orderActivity.getOrderActivities({
+				orderActivityFilter: {
+					"@type": "by_item",
+					contract: toAddress(conf.testErc721.options.address),
+					tokenId: toBigNumber("1"),
+					types: [OrderActivityFilterByItemTypes.MATCH,
+						OrderActivityFilterByItemTypes.LIST,
+						OrderActivityFilterByItemTypes.BID],
+				},
+			})
+			expect(a.items.filter(a => a["@type"] === "bid")).toHaveLength(1)
+			expect(a.items.filter(a => a["@type"] === "match")).toHaveLength(1)
+
+			const b = await sdk2.apis.orderActivity.getOrderActivities({
+				orderActivityFilter: {
+					"@type": "by_item",
+					contract: toAddress(conf.testErc721.options.address),
+					tokenId: toBigNumber("2"),
+					types: [OrderActivityFilterByItemTypes.MATCH,
+						OrderActivityFilterByItemTypes.LIST,
+						OrderActivityFilterByItemTypes.BID],
+				},
+			})
+			expect(b.items.filter(a => a["@type"] === "bid")).toHaveLength(1)
+			expect(b.items.filter(a => a["@type"] === "match")).toHaveLength(1)
+		})
+	})
+})
